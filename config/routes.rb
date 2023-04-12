f9a4fc452596a56f8c0de7421383baaae34350b1
EC2 Default User <ec2-user@ip-172-31-9-218.ap-northeast-1.compute.internal>
2023-04-12 08:05:01 +0000

    12章

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  get 'books/new'
+   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'books/edit'
   get 'top' => 'homes#top'
-　resources :books
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post 'books' => 'books#create'
+  get 'books' => 'books#index'
+  get 'books/:id' => 'books#show’, as: 'book'
+  
 end
+
