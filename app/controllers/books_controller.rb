2e3809ee86c38170dc6529102af8d04e860c75bd
EC2 Default User <ec2-user@ip-172-31-9-218.ap-northeast-1.compute.internal>
2023-04-12 13:30:29 +0000

    課題2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,6 +11,8 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
+    #↑この１行追記
   end
 
   def show
@@ -18,9 +20,22 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
-
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+  
+  
   private
 
   def book_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  get 'books/new'
-   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'books/edit'
+    
   get 'top' => 'homes#top'
-  post 'books' => 'books#create'
-  get 'books' => 'books#index'
-  get 'books/:id' => 'books#show’, as: 'book'
+  resources :books
   
-end
+ end
+
 
